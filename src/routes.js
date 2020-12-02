c0dde708fad07d322196ba6debf0e1e9a46a05fb
Ewerton Maciel <ewertoncz@gmail.com>
2020-12-01 22:22:33 -0300

    Definindo rotas

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const routes = require("express").Router();
+const ProductController = require("./controllers/ProductController");
+
+routes.get("/product", ProductController.index);
+routes.get("/product:id", ProductController.show);
+routes.post("/product", ProductController.store);
+routes.put("/product:id", ProductController.update);
+routes.delete("/product:id", ProductController.destroy);
+
+module.exports = routes;
